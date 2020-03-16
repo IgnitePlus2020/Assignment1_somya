b712ff3326805d484ef9c4195fb8239a32262bba
somya1209 <somya123024@gmail.com>
2020-03-17 01:05:12 +0530

    series neg odd/pos even

---
+++ src/com/tgt/igniteplus/Seq_q12.java
@@ -0,0 +1,24 @@
+package com.tgt.igniteplus;
+
+import java.util.Scanner;
+
+public class Seq_q12 {
+    public  static  void  main(String args[])
+    {
+
+        int n;
+        Scanner in  =new Scanner(System.in);
+        n=in.nextInt();
+        for (int i = 1; i <=n ; i++) {
+          if(i%2==0)
+              System.out.print(i+", ");
+          else System.out.print(-1*i+", ");
+
+
+        }
+    }
+}
+/*
+10
+-1, 2, -3, 4, -5, 6, -7, 8, -9, 10,
+ */
