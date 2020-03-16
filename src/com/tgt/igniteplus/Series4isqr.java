29876570b47979701f222c358303bf226709049e
somya1209 <somya123024@gmail.com>
2020-03-17 00:58:25 +0530

    series calculating 4*i^2

---
+++ src/com/tgt/igniteplus/Series4isqr.java
@@ -0,0 +1,20 @@
+package com.tgt.igniteplus;
+
+import java.util.Scanner;
+
+public class Series4isqr {
+    public static void main(String[]args){
+        int n;
+        Scanner in  =new Scanner(System.in);
+        n=in.nextInt();
+        for (int i = 1; i <=n ; i++) {
+            System.out.print(4*i*i+", ");
+
+        }
+
+    }
+}
+/*
+10
+4, 16, 36, 64, 100, 144, 196, 256, 324, 400,
+ */
