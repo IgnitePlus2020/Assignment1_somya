c3099aff41206ff81b826c73a087c6969031726c
somya1209 <somya123024@gmail.com>
2020-03-24 15:23:38 +0530

    improved output format for q12

--- src/com/tgt/igniteplus/Seq_q12.java
+++ src/com/tgt/igniteplus/Seq_q12.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class Seq_q12 {
     public  static  void  main(String args[])
     {
-
         int n;
         Scanner in  =new Scanner(System.in);
         n=in.nextInt();
         for (int i = 1; i <=n ; i++) {
-          if(i%2==0)
-              System.out.print(i+", ");
-          else System.out.print(-1*i+", ");
-
-
+            if(i%2==0)
+              System.out.print(i);
+        else
+              System.out.print(-1*i);
+        if (i<n)
+              System.out.print(",");
         }
     }
 }
 /*
 10
--1, 2, -3, 4, -5, 6, -7, 8, -9, 10,
+-1,2,-3,4,-5,6,-7,8,-9,10
  */
